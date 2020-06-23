a3ea35d65482043942f1e5697eda10146e5157ae
Yev Ro <yev.rokhin@gmail.com>
2020-06-23 16:36:02 -0700

    added store js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,24 +1,24 @@
 const app = require("express").Router();
 
-app.get("/api/notes", (req, res) => {
+app.get("/notes", (req, res) => {
     store
         .getNotes()
         .then((notes) => res.json(notes))
-        .catch((err) => res.status(404).json(err));
+        .catch((err) => res.status(500).json(err));
 });
 
-app.post("/api/notes", (req, res) => {
+app.post("/notes", (req, res) => {
     store
         .addNote()
         .then((note) => res.json(note))
-        .catch((err) => res.status(404).json(err));
+        .catch((err) => res.status(500).json(err));
 });
 
-app.delete("/api/notes", (req, res) => {
+app.delete("/notes", (req, res) => {
     store
         .removeNote()
         .then(() => res.json({ ok: true }))
-        .catch((err) => res.status(404).json(err));
+        .catch((err) => res.status(500).json(err));
 });
 
 module.exports = app;
